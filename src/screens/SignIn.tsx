a168872fc996b85a813157929071ad8b0719f907
Felipe Decome <decome.felipe@gmail.com>
2024-08-06 10:18:25 -0300

    feat: adds input component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,7 +1,8 @@
-import { Center, Image, Text, VStack } from "@gluestack-ui/themed";
+import { Center, Heading, Image, Text, VStack } from "@gluestack-ui/themed";
 
 import backgroundImageSrc from "@assets/background.png";
 import Logo from "@assets/logo.svg";
+import { Input } from "@components/Input";
 
 export function SignIn() {
 	// const { signIn } = useAuth();
@@ -26,13 +27,22 @@ export function SignIn() {
 				w="$full"
 			/>
 
-			<Center my="$24">
-				<Logo />
+			<VStack flex={1} pb={"$16"} px={"$10"}>
+				<Center my="$24">
+					<Logo />
 
-				<Text color="$gray100" fontSize="$sm">
-					Treine sua mente e o seu corpo.
-				</Text>
-			</Center>
+					<Text color="$gray100" fontSize="$sm">
+						Treine sua mente e o seu corpo.
+					</Text>
+				</Center>
+
+				<Center gap="$2">
+					<Heading color="$gray100">Acesse a conta</Heading>
+
+					<Input placeholder="Email" />
+					<Input placeholder="Senha" />
+				</Center>
+			</VStack>
 		</VStack>
 	);
 }
