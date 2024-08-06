25f0700f7e119542f6acd95cc146fc1e39456898
Felipe Decome <decome.felipe@gmail.com>
2024-08-06 15:55:40 -0300

    feat(screens/profile): adds header

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,43 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { Center, Text, VStack } from "@gluestack-ui/themed";
+import { ScrollView, TouchableOpacity } from "react-native";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+
+import { gluestackUIConfig } from "../../config/gluestack-ui.config";
 
 export function Profile() {
 	return (
-		<Center flex={1}>
-			<Text color="white">Profile</Text>
-		</Center>
+		<VStack flex={1}>
+			<ScreenHeader title="Perfil" />
+
+			<ScrollView
+				contentContainerStyle={{
+					paddingBottom: gluestackUIConfig.tokens.space["9"],
+				}}
+			>
+				<Center mt="$6" px="$10">
+					<UserPhoto
+						alt="Imagem do usuário"
+						source={{
+							uri: "https://github.com/felipedecome.png",
+						}}
+						size="xl"
+					/>
+
+					<TouchableOpacity>
+						<Text
+							color="$green500"
+							fontSize="$md"
+							fontFamily="$heading"
+							mb="$8"
+							mt="$2"
+						>
+							Alterar Foto
+						</Text>
+					</TouchableOpacity>
+				</Center>
+			</ScrollView>
+		</VStack>
 	);
 }
