10f71ed550339c2d14965b29090ec416099ca3a1
Felipe Decome <decome.felipe@gmail.com>
2024-08-06 10:39:53 -0300

    feat(screens/signin): adds buttons to form

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,7 +1,15 @@
-import { Center, Heading, Image, Text, VStack } from "@gluestack-ui/themed";
+import {
+	Center,
+	Heading,
+	Image,
+	ScrollView,
+	Text,
+	VStack,
+} from "@gluestack-ui/themed";
 
 import backgroundImageSrc from "@assets/background.png";
 import Logo from "@assets/logo.svg";
+import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 export function SignIn() {
@@ -18,35 +26,52 @@ export function SignIn() {
 	// };
 
 	return (
-		<VStack bg="$gray700" flex={1}>
-			<Image
-				alt="Pessoas treinando"
-				h={624}
-				position="absolute"
-				source={backgroundImageSrc}
-				w="$full"
-			/>
+		<ScrollView
+			contentContainerStyle={{
+				flexGrow: 1,
+			}}
+			showsVerticalScrollIndicator={false}
+		>
+			<VStack bg="$gray700" flex={1}>
+				<Image
+					alt="Pessoas treinando"
+					h={624}
+					position="absolute"
+					source={backgroundImageSrc}
+					w="$full"
+				/>
 
-			<VStack flex={1} pb={"$16"} px={"$10"}>
-				<Center my="$24">
-					<Logo />
+				<VStack flex={1} pb={"$16"} px={"$10"}>
+					<Center my="$24">
+						<Logo />
 
-					<Text color="$gray100" fontSize="$sm">
-						Treine sua mente e o seu corpo.
-					</Text>
-				</Center>
+						<Text color="$gray100" fontSize="$sm">
+							Treine sua mente e o seu corpo.
+						</Text>
+					</Center>
 
-				<Center gap="$2">
-					<Heading color="$gray100">Acesse a conta</Heading>
+					<Center gap="$2">
+						<Heading color="$gray100">Acesse a conta</Heading>
 
-					<Input
-						placeholder="Email"
-						keyboardType="email-address"
-						autoCapitalize="none"
-					/>
-					<Input placeholder="Senha" secureTextEntry />
-				</Center>
+						<Input
+							placeholder="Email"
+							keyboardType="email-address"
+							autoCapitalize="none"
+						/>
+						<Input placeholder="Senha" secureTextEntry />
+
+						<Button title="Acessar" />
+					</Center>
+
+					<Center flex={1} justifyContent="flex-end" mt="$4">
+						<Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
+							Ainda não tem acesso?
+						</Text>
+
+						<Button title="Criar conta" variant="outline" />
+					</Center>
+				</VStack>
 			</VStack>
-		</VStack>
+		</ScrollView>
 	);
 }
