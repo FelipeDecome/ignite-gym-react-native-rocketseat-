660fcf78b6a0f45baba5118920ac052cd3539759
Felipe Decome <decome.felipe@gmail.com>
2024-08-07 11:06:18 -0300

    feat(screens/signup): adding simple validations

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,33 +1,58 @@
-import { Input as GluestackInput, InputField } from "@gluestack-ui/themed";
+import {
+	FormControl,
+	FormControlError,
+	FormControlErrorText,
+	Input as GluestackInput,
+	InputField,
+} from "@gluestack-ui/themed";
 import type { ComponentProps } from "react";
 
 type Props = ComponentProps<typeof InputField> & {
+	errorMessage?: string;
+	isInvalid?: boolean;
 	islight?: boolean;
 	isReadOnly?: boolean;
 };
 
-export function Input({ islight = false, isReadOnly = false, ...rest }: Props) {
+export function Input({
+	errorMessage,
+	isInvalid: isInvalidProp = false,
+	islight = false,
+	isReadOnly = false,
+	...rest
+}: Props) {
+	const isInvalid = isInvalidProp || !!errorMessage;
+
 	return (
-		<GluestackInput
-			bg={islight ? "$gray600" : "$gray700"}
-			borderColor="transparent"
-			borderRadius="$md"
-			borderWidth="$1"
-			h="$14"
-			px="$4"
-			$focus={{
-				borderWidth: "$1",
-				borderColor: "$green500",
-			}}
-			isReadOnly={isReadOnly}
-			opacity={isReadOnly ? 0.5 : 1}
-		>
-			<InputField
-				color="$white"
-				fontFamily="$body"
-				placeholderTextColor="$gray300"
-				{...rest}
-			/>
-		</GluestackInput>
+		<FormControl isInvalid={isInvalid} w="$full">
+			<GluestackInput
+				bg={islight ? "$gray600" : "$gray700"}
+				borderColor="transparent"
+				borderRadius="$md"
+				borderWidth="$1"
+				isInvalid={isInvalid}
+				isReadOnly={isReadOnly}
+				h="$14"
+				opacity={isReadOnly ? 0.5 : 1}
+				px="$4"
+				$focus-borderColor={isInvalid ? "$red500" : "$green500"}
+				$invalid-borderColor="$red500"
+			>
+				<InputField
+					color="$white"
+					fontFamily="$body"
+					placeholderTextColor="$gray300"
+					{...rest}
+				/>
+			</GluestackInput>
+
+			{errorMessage && (
+				<FormControlError>
+					<FormControlErrorText color="$red500">
+						{errorMessage}
+					</FormControlErrorText>
+				</FormControlError>
+			)}
+		</FormControl>
 	);
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -7,17 +7,31 @@ import {
 	VStack,
 } from "@gluestack-ui/themed";
 import { useNavigation } from "@react-navigation/native";
+import { Controller, useForm } from "react-hook-form";
 
 import backgroundImageSrc from "@assets/background.png";
 import Logo from "@assets/logo.svg";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
+type SignUpFormData = {
+	name: string;
+	email: string;
+	password: string;
+	confirmPassword: string;
+};
+
 export function SignUp() {
 	const navigation = useNavigation();
+	const { control, formState, handleSubmit } = useForm<SignUpFormData>({});
+	const { errors } = formState;
 
 	function handleGoBack() {
 		navigation.goBack();
+	}
+
+	function handleSignUp(data: SignUpFormData) {
+		console.log(data);
 	}
 
 	return (
@@ -45,22 +59,85 @@ export function SignUp() {
 						</Text>
 					</Center>
 
-					<Center flex={1} gap="$4">
+					<Center flex={1} gap="$3">
 						<Heading color="$gray100">Crie sua conta</Heading>
 
-						<Input placeholder="Nome" />
-
-						<Input
-							placeholder="Email"
-							keyboardType="email-address"
-							autoCapitalize="none"
+						<Controller
+							control={control}
+							name="name"
+							render={({ field: { onChange, value } }) => (
+								<Input
+									errorMessage={errors.name?.message}
+									isInvalid={!!errors.name}
+									onChangeText={onChange}
+									placeholder="Nome"
+									value={value}
+								/>
+							)}
+							rules={{
+								required: "Campo obrigatório",
+							}}
 						/>
 
-						<Input placeholder="Senha" secureTextEntry />
+						<Controller
+							control={control}
+							name="email"
+							render={({ field: { onChange, value } }) => (
+								<Input
+									autoCapitalize="none"
+									errorMessage={errors.email?.message}
+									isInvalid={!!errors.email}
+									keyboardType="email-address"
+									onChangeText={onChange}
+									placeholder="Email"
+									value={value}
+								/>
+							)}
+							rules={{
+								required: "Campo obrigatório",
+								pattern: {
+									value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i,
+									message: "Email inválido",
+								},
+							}}
+						/>
 
-						<Input placeholder="Confirmar a Senha" secureTextEntry />
+						<Controller
+							control={control}
+							name="password"
+							render={({ field: { onChange, value } }) => (
+								<Input
+									errorMessage={errors.password?.message}
+									isInvalid={!!errors.password}
+									onChangeText={onChange}
+									placeholder="Senha"
+									secureTextEntry
+									value={value}
+								/>
+							)}
+						/>
 
-						<Button title="Criar e acessar" />
+						<Controller
+							control={control}
+							name="confirmPassword"
+							render={({ field: { onChange, value } }) => (
+								<Input
+									errorMessage={errors.confirmPassword?.message}
+									isInvalid={!!errors.confirmPassword}
+									onChangeText={onChange}
+									onSubmitEditing={handleSubmit(handleSignUp)}
+									placeholder="Confirmar a Senha"
+									returnKeyType="send"
+									secureTextEntry
+									value={value}
+								/>
+							)}
+						/>
+
+						<Button
+							onPress={handleSubmit(handleSignUp)}
+							title="Criar e acessar"
+						/>
 					</Center>
 
 					<Button
